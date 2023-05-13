5bf5479302f665e986a19385b6b365e466ca4fbe
gabewolford <gabewolford@gmail.com>
2023-05-13 10:17:27 -0700

    added footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <>
+            <footer className="absolute bottom-0">
+                <h1 className='text-3xl text-blue-500'>Islamorda Fishing Guides Footer</h1>
+            </footer>
+        </>
+    )
+}
