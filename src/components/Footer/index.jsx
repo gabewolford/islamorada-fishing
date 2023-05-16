0f95e37ffccf05c875ae08f9f5f0fc2b7103f899
gabewolford <gabewolford@gmail.com>
2023-05-15 18:29:37 -0700

    logo now in its own column, copyright moved to bottom on all screens

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,32 +6,22 @@ export default function Footer() {
     return (
         <>
             <footer className="w-full p-10 px-4 sm:px-6 lg:px-8 mx-auto bg-cyan-69 absolute bottom-0">
-
                 <div className="grid grid-cols-2 lg:grid-cols-4 gap-6">
-                    
-                    <div className="col-span-full lg:col-span-1 lg:block">
+                    <div className="col-span-full lg:col-span-1">
                         <Link className="flex-none text-xl text-muted-420 font-semibold " to="/" aria-label="Brand">logo</Link>
-                        <p className="mt-3 text-xs text-muted-420">© 2023 IslamoradaFishingGuidesandCharters</p>
-                        <p className="mt-3 text-xs text-muted-420">All rights reserved.</p>
                     </div>
-
-
                     <div className="mt-3 grid space-y-3 text-sm">
                         <p><Link className="inline-flex gap-x-2 text-white" to="/charters">Fishing Charters</Link></p>
                         <p><Link className="inline-flex gap-x-2 text-white" to="/charters/inshore-backcountry">Backcountry & Inshore</Link></p>
                         <p><Link className="inline-flex gap-x-2 text-white" to="/charters/offshore">Reef & Offshore</Link></p>
                         <p><Link className="inline-flex gap-x-2 text-white" to="/charters/eco-enviro-tours">Eco & Enviro Tours</Link></p>
                     </div>
-
-
                     <div className="mt-3 grid space-y-3 text-sm">
                         <p><Link className="inline-flex gap-x-2 text-white" to="/guides">Guides</Link></p>
                         <p><Link className="inline-flex gap-x-2 text-white" to="/visit-islamorada">Explore Islamorada</Link></p>
                         <p><Link className="inline-flex gap-x-2 text-white" to="/frequently-asked-questions">FAQs</Link></p>
                         <p><Link className="inline-flex gap-x-2 text-white" to="/partners">Our Partners</Link></p>
                     </div>
-
-
                     <div className="col-span-full lg:col-span-1 mt-3 grid space-y-3 text-sm">
                         <p><Link className="inline-flex gap-x-2 text-white" to="/contact">Contact Us</Link></p>
                         <p className="inline-flex gap-x-2 text-white">Call us today <span className="text-white font-bold">305-394-7484</span></p>
@@ -40,7 +30,9 @@ export default function Footer() {
                             <a href="https://www.instagram.com/islamoradafishingguides/"><span><img src={instagramLogo}/></span></a>
                         </p>
                     </div>
-
+                    <div className="col-span-full col-span-2">
+                        <p className="mt-3 text-xs text-muted-420">© 2023 IslamoradaFishingGuidesandCharters. All rights reserved</p>
+                    </div>
                 </div>
 
             </footer>
