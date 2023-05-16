2914f5c60752598204e160dc3a413d22135cbd88
gabewolford <gabewolford@gmail.com>
2023-05-16 16:46:42 -0700

    created partner header component

--- src/components/PartnersPage/index.jsx
+++ src/components/PartnersPage/index.jsx
@@ -1,9 +1,16 @@
+import Header from "../PartnersComponents/Header"
+
 export default function PartnersPage() {
+
     return (
         <>
-            <div className="mx-auto pt-14">
-                <h3>Partners Page</h3>
+            <div className="mx-auto max-w-7xl pt-24">
+                <div className="mx-6 lg:mx-20 mb-0 md:mb-8">
+                    <Header />
+                </div>
             </div>
+            
+    
         </>
-    )
+    )      
 }
