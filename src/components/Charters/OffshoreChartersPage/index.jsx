d629aee8e216b478ba75f0499ab16b6f872a4193
gabewolford <gabewolford@gmail.com>
2023-05-16 11:22:06 -0700

    stubbed up charters index and detail pages

--- src/components/Charters/OffshoreChartersPage/index.jsx
+++ src/components/Charters/OffshoreChartersPage/index.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom"
 export default function OffshoreChartersPage() {
     return (
         <>
-        <div className="absolute top-36 mx-20">
-            <h3 className='text-xl text-blue-500'>Reef & Offshore Charters Page</h3>
-            <Link to="/charters" className="border-2">Back to all charters</Link>
-        </div>
+            <div className="mx-auto max-w-7xl pt-24">
+                <div className="mx-6 lg:mx-20 mb-16">
+                    <h1 className="text-3xl font-medium mb-6">Offshore Charters</h1>
+                    <Link to="/charters" className="text-cyan-420 hover:text-cyan-hover text-cyan-420 hover:text-cyan-hover transition duration-100 ease-in-out font-medium">← Back to all charters</Link>
+                </div>
+            </div>
         </>
     )
 }
