3f30b4fd97e37e230f3217d82d1aa51b88faf37a
gabewolford <gabewolford@gmail.com>
2023-05-15 22:29:59 -0700

    increased teext size for medium screens and up

--- src/components/HomeComponents/PhotoLeftInfoCard/index.jsx
+++ src/components/HomeComponents/PhotoLeftInfoCard/index.jsx
@@ -9,8 +9,8 @@ export default function PhotoLeftInfoCard() {
                     <img src={bookdirectlyImage} alt="Hero Image" />
                 </div>
                 <div className="flex flex-col lg:mr-20">
-                    <h4 className="text-2xl font-medium leading-7 mb-4 ">Book directly with one of our local guides</h4>
-                    <p className="text-sm mb-4">All of our guides are licensed captains with years of experience and local expertise. We welcome all experience levels, whether you’re a seasoned fly fisherman or a family looking to try something new.</p>
+                    <h4 className="text-2xl md:text-3xl font-medium leading-7 mb-4 ">Book directly with one of our local guides</h4>
+                    <p className="text-sm md:text-base mb-4">All of our guides are licensed captains with years of experience and local expertise. We welcome all experience levels, whether you’re a seasoned fly fisherman or a family looking to try something new.</p>
                     <Link to="/guides" className="text-white font-medium bg-cyan-420 hover:bg-cyan-hover transition duration-300 ease-in-out rounded-3xl px-5 py-3.5 w-fit">Meet Our Guides</Link>
                 </div>
             </div>
