5e87bed75fae2879f0b4d29c9f6f6cc1c7722c75
gabewolford <gabewolford@gmail.com>
2023-05-15 22:04:55 -0700

    contact banner complete and responsive

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,8 +7,15 @@ export default function Footer() {
     return (
         <>
             <footer className="w-full mx-auto bg-cyan-69 absolute bottom-0">
-                <div className="w-full">
-                    <img className='h-[350px] w-full' src={waterImage} alt="Contact Us" />
+                <div className="flex flex-col justify-center items-center gap-4">
+                    <div className="relative w-full">
+                        <img className='h-[350px] w-full' src={waterImage} alt="Contact Us" />
+                    </div>
+                    <div className="absolute px-6 flex flex-col text-center">
+                        <h4 className="text-2xl md:text-3xl text-white font-medium mb-4">Plan your custom fishing charter with our help.</h4>
+                        <h6 className="text-base md:text-lg text-white mb-4">Not sure which charter or guide is right for you? Have questions? Talk with our experts who will help you plan and book your perfect Florida Keys fishing trip today!</h6>
+                        <Link to="/contact" className="text-cyan-420 font-medium bg-white hover:bg-cyan-hover hover:text-white transition duration-300 ease-in-out rounded-3xl px-5 py-3.5 block w-fit mx-auto">Contact Us</Link>
+                    </div>
                 </div>
                 <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 p-10 sm:px-6 lg:px-8">
                     <div className="col-span-full lg:col-span-1">
