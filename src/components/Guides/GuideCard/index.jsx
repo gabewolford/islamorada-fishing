b9c1e4e628b4f229e55556b6837ea373115b265c
gabewolford <gabewolford@gmail.com>
2023-06-13 21:49:09 -0700

    updated guide card component, added badges and conditions

--- src/components/Guides/GuideCard/index.jsx
+++ src/components/Guides/GuideCard/index.jsx
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom"
 
 export default function GuideCard({ guideData, setDetailPage }) {
-    let guideName, guidePhoto, offshoreBadge, inshoreBadge
+    let guideName, guidePhoto, badges
     if (guideData) {
-        guideName = guideData.name
+        guideName = <h3 className="text-md mt-2 text-center font-bold">Capt. {guideData.name}</h3>
         guidePhoto = '..' + guideData.featured_pic
+        if (guideData.backcountry === true && guideData.offshore === true) {
+            badges = <h5 className="text-center">
+                        <span className="text-center text-xs bg-cyan-420 text-white px-2 mr-1 py-1 rounded-full">Offshore</span> 
+                        <span className="text-center text-xs bg-cyan-69 text-white px-2 py-1 rounded-full">Backcountry</span>
+                    </h5>
+        } else if (guideData.backcountry === true) {
+            badges = <h5 className="text-center">
+                        <span className="text-center text-xs bg-cyan-69 text-white px-2 py-1 rounded-full">Backcountry</span>
+                    </h5>
+        } else if (guideData.offshore === true) {
+            badges = <h5 className="text-center">
+                        <span className="text-center text-xs bg-cyan-420 text-white px-2 mr-1 py-1 rounded-full">Offshore</span> 
+                    </h5>
+        }
     }
 
     return (
@@ -15,7 +29,8 @@ export default function GuideCard({ guideData, setDetailPage }) {
                 className="p-4"
             >
                 <img src={guidePhoto} alt="" />
-                <h3 className="text-md mt-2 text-center font-semi-bold">Capt. {guideName}</h3>
+                {guideName}
+                {badges}
             </Link>
         </>
     )
