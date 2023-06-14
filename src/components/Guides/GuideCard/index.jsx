b1f4e09fdb8d8e390e40be86664c39598b95f578
gabewolford <gabewolford@gmail.com>
2023-06-13 17:37:56 -0700

    updated guide cards layout and replaced placeholder image

--- src/components/Guides/GuideCard/index.jsx
+++ src/components/Guides/GuideCard/index.jsx
@@ -1,15 +1,10 @@
 import { Link } from "react-router-dom"
 
 export default function GuideCard({ guideData, setDetailPage }) {
-
-    let backcountryBadge
-    if (guideData.backcountry === true ) {
-        backcountryBadge = <p className="absolute bottom-0 left-0 text-sm bg-green-400 m-2 px-2 py-1 rounded-xl">Backcountry</p>
-    }
-
-    let offshoreBadge
-    if (guideData.offshore === true ) {
-        offshoreBadge = <p className="absolute bottom-0 right-0 text-sm bg-orange-300 m-2 px-2 py-1 rounded-xl">Offshore</p>
+    let guideName, guidePhoto, offshoreBadge, inshoreBadge
+    if (guideData) {
+        guideName = guideData.name
+        guidePhoto = '..' + guideData.featured_pic
     }
 
     return (
@@ -17,17 +12,11 @@ export default function GuideCard({ guideData, setDetailPage }) {
             <Link 
                 to={`/guides/${guideData.slug}`}
                 onClick={() => setDetailPage(guideData)}
+                className="p-4"
             >
-                <div className="border-2 border-cyan-420 w-fit mb-4">
-                    <h3 className="text-xl">Captain {guideData.name}</h3>
-                    <div className="relative">
-                        <img src={guideData.featured_pic} alt={guideData.name} />
-                        {backcountryBadge}
-                        {offshoreBadge}
-                    </div>
-                </div>
+                <img src={guidePhoto} alt="" />
+                <h3 className="text-md mt-2 text-center font-semi-bold">Capt. {guideName}</h3>
             </Link>
-            
         </>
     )
 }
