a58cfd7fbc7f73a8e452bd5124321ddb2eaf976a
gabewolford <gabewolford@gmail.com>
2023-06-15 16:46:33 -0700

    created partner card component

---
+++ src/components/PartnersComponents/PartnerCard/index.jsx
@@ -0,0 +1,21 @@
+export default function PartnerCard({ partnerData }) {
+    let partnerName, partnerPhoto, partnerURL
+    if (partnerData) {
+        partnerName = <h3 className="text-sm text-center font-bold mt-2">{partnerData.name}</h3>
+        partnerPhoto = '../' + partnerData.image
+        partnerURL = partnerData.url
+    }
+
+    return (
+        <>
+            <a
+                href={partnerURL}
+                target="_blank"
+                className="p-4 hover-grow"
+            >
+                <img src={partnerPhoto} alt={partnerName} className="mx-auto"/>
+                {partnerName}
+            </a>
+        </>
+    )
+}
