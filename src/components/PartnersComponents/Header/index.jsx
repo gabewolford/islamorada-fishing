7b24715788176dabf2a7956b77287d93bfcfa072
gabewolford <gabewolford@gmail.com>
2023-06-15 16:46:09 -0700

    fixed margins

--- src/components/PartnersComponents/Header/index.jsx
+++ src/components/PartnersComponents/Header/index.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function Header() {
     return (
         <>
-            <div className="flex flex-col items-start gap-3 mb-8 md:mb-16">
+            <div className="flex flex-col items-start gap-3 mb-4 md:mb-8">
                 <p className="text-sm"><Link to="/" className="hover:text-cyan-hover">Home</Link> / Partners</p>
                 <h1 className="text-3xl lg:text-5xl font-medium">Our Partners</h1>
             </div>
