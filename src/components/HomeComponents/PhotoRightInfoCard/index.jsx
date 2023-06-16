9465fcf4d2c22afa85d1c0c839174ee307ec68f2
gabewolford <gabewolford@gmail.com>
2023-06-16 10:17:26 -0700

    fixed link to explore

--- src/components/HomeComponents/PhotoRightInfoCard/index.jsx
+++ src/components/HomeComponents/PhotoRightInfoCard/index.jsx
@@ -11,7 +11,7 @@ export default function PhotoRightInfoCard() {
                 <div className="flex flex-col lg:ml-20 order-0">
                     <h4 className="text-2xl md:text-3xl font-medium leading-7 mb-4 ">Meet us at World Wide Sportsman</h4>
                     <p className="text-sm md:text-base mb-4">Our fishing charters begin and end at World Wide Sportsman Bayside Marina, the finest full service marina in the Florida Keys. Learn more about our marina and where to stay any play while in town.</p>
-                    <Link to="/visit-islamorada" className="text-white font-medium bg-cyan-420 hover:bg-cyan-hover transition duration-300 ease-in-out rounded-3xl px-5 py-3.5 w-fit">Explore Islamorada</Link>
+                    <Link to="/explore-islamorada" className="text-white font-medium bg-cyan-420 hover:bg-cyan-hover transition duration-300 ease-in-out rounded-3xl px-5 py-3.5 w-fit">Explore Islamorada</Link>
                 </div>
              </div>
         </>
