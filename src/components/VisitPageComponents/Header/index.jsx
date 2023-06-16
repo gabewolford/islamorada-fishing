d641b706241c3d5f43ad415284392013dcf0516d
gabewolford <gabewolford@gmail.com>
2023-06-16 13:17:23 -0700

    changed visit to explore in breadcrumbs

--- src/components/VisitPageComponents/Header/index.jsx
+++ src/components/VisitPageComponents/Header/index.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <>
             <div className="flex flex-col items-start gap-3 mb-8 md:mb-16">
-                <p className="text-sm"><Link to="/" className="hover:text-cyan-hover">Home</Link> / Visit Islamorada</p>
+                <p className="text-sm"><Link to="/" className="hover:text-cyan-hover">Home</Link> / Explore Islamorada</p>
                 <h1 className="text-3xl lg:text-5xl font-medium">Explore Islamorada</h1>
             </div>
         </>
