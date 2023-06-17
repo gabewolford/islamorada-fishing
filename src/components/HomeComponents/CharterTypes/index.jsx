a3aae0916d478dedddd687c14b187bc9ef308f65
gabewolford <gabewolford@gmail.com>
2023-06-17 08:03:11 -0700

    added w and h for images

--- src/components/HomeComponents/CharterTypes/index.jsx
+++ src/components/HomeComponents/CharterTypes/index.jsx
@@ -13,30 +13,30 @@ export default function CharterTypes() {
                     <div className="flex flex-col md:flex-row md:grid md:grid-cols-3 md:flex-wrap md:gap-6 items-center self-stretch">
                         <div className="flex items-center md:flex-col self-stretch mb-6 gap-4">
                             <div className="w-2/5 md:w-auto">
-                                <img className="" src={backcountryImage} alt="backcountry"/>
+                                <img className="w-full h-full" src={backcountryImage} alt="backcountry"/>
                             </div>
                             <div className="flex flex-col items-start w-3/5 md:w-full gap-2">
-                                <h4 className="text-base md:text-lg font-medium">Backcountry & Inshore</h4>
+                                <h3 className="text-base md:text-lg font-medium">Backcountry & Inshore</h3>
                                 <p className="text-sm">Snook, redfish, tarpon and more</p>
                                 <Link to="/charters/inshore-backcountry" className="text-cyan-420 hover:text-cyan-hover transition duration-100 ease-in-out text-sm font-medium">Explore Inshore</Link>
                             </div>
                         </div>
                         <div className="flex items-center md:flex-col self-stretch mb-6 gap-4">
                             <div className="w-2/5 md:w-auto">
-                                <img className="" src={offshoreImage} alt="offshore"/>
+                                <img className="w-full h-full" src={offshoreImage} alt="offshore"/>
                             </div>
                             <div className="flex flex-col items-start w-3/5 md:w-full gap-2">
-                                <h4 className="text-base md:text-lg font-medium">Reef & Offshore</h4>
+                                <h3 className="text-base md:text-lg font-medium">Reef & Offshore</h3>
                                 <p className="text-sm">Sailfish, wahoo, tuna and more</p>
                                 <Link to="/charters/offshore" className="text-cyan-420 hover:text-cyan-hover transition duration-100 ease-in-out text-sm font-medium">Explore Offshore</Link>
                             </div>
                         </div>
                         <div className="flex items-center md:flex-col self-stretch md:mb-6 gap-4">
                             <div className="w-2/5 md:w-auto">
-                                <img className="" src={ecotoursImage} alt="eco/enviro"/>
+                                <img className="w-full h-full" src={ecotoursImage} alt="eco/enviro"/>
                             </div>
                             <div className="flex flex-col items-start w-3/5 md:w-full gap-2">
-                                <h4 className="text-base md:text-lg font-medium">Eco & Enviro Tours</h4>
+                                <h3 className="text-base md:text-lg font-medium">Eco & Enviro Tours</h3>
                                 <p className="text-sm">Backcountry and beach trips</p>
                                 <Link to="/charters/eco-enviro-tours" className="text-cyan-420 hover:text-cyan-hover transition duration-100 ease-in-out text-sm font-medium">Explore Eco Tours</Link>
                             </div>
