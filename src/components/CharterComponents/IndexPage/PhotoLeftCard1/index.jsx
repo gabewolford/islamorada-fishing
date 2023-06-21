c1f2bf29a29266f0085e310a8f6db76d5584cd31
gabewolford <gabewolford@gmail.com>
2023-06-20 18:05:33 -0700

    updated font weights for charter index page components

--- src/components/CharterComponents/IndexPage/PhotoLeftCard1/index.jsx
+++ src/components/CharterComponents/IndexPage/PhotoLeftCard1/index.jsx
@@ -7,9 +7,9 @@ export default function PhotoLeftCard1() {
             <div className="fade-in show flex flex-col md:flex-row items-center gap-6 lg:gap-28 order-1 mb-16">
                 <img className="md:w-1/2" src={backcountryImage} alt="Backcountry Image" />
                 <div className="flex flex-col lg:mr-20">
-                    <h4 className="text-2xl md:text-3xl font-medium leading-7 mb-4 ">Backcountry & Inshore</h4>
+                    <h4 className="text-2xl md:text-3xl medium leading-7 mb-4 ">Backcountry & Inshore</h4>
                     <p className="text-sm md:text-base mb-4">Some of the premiere fishing areas in the Florida Keys are on the famous Islamorada flats. Bonefish, tarpon, redfish, snook, permit are the catch-of-the-day here, but expect barracuda, shark and other species to visit too.</p>
-                    <Link to="/charters/inshore-backcountry" className="text-white font-medium bg-cyan-420 hover:bg-cyan-hover transition duration-300 ease-in-out rounded-3xl px-5 py-3.5 w-fit">View Inshore Packages</Link>
+                    <Link to="/charters/inshore-backcountry" className="text-white medium bg-cyan-420 hover:bg-cyan-hover transition duration-300 ease-in-out rounded-3xl px-5 py-3.5 w-fit">View Inshore Packages</Link>
                 </div>
             </div>
         </>
