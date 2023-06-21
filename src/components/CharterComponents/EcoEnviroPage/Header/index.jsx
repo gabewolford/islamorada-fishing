d05f04e8cdb591ab9d39e5ba8e2b821190794129
gabewolford <gabewolford@gmail.com>
2023-06-20 18:12:01 -0700

    updated font weights on eco/enviro page components

--- src/components/CharterComponents/EcoEnviroPage/Header/index.jsx
+++ src/components/CharterComponents/EcoEnviroPage/Header/index.jsx
@@ -5,7 +5,7 @@ export default function Header() {
         <>
             <div className="flex flex-col items-start gap-3 mb-8 md:mb-16">
                 <p className="text-sm"><Link to="/" className="hover:text-cyan-hover">Home</Link> / <Link to="/charters" className="hover:text-cyan-hover">Charters</Link> / Eco & Enviro Tours</p>
-                <h1 className="text-3xl lg:text-5xl font-medium">Eco & Enviro Tour Packages</h1>
+                <h1 className="text-3xl lg:text-5xl bold">Eco & Enviro Tour Packages</h1>
             </div>
         </>
     )
--- src/components/CharterComponents/EcoEnviroPage/PhotoRightCard2/index.jsx
+++ src/components/CharterComponents/EcoEnviroPage/PhotoRightCard2/index.jsx
@@ -29,12 +29,12 @@ export default function PhotoRightCard2() {
             <div className="fade-in flex flex-col md:flex-row items-center gap-6 lg:gap-28 order-1 mb-16">
                 <img className="md:w-1/2 md:order-1" src={sandbarImage} alt="Sandbar Image" />
                 <div className="flex flex-col md:order-0 lg:ml-20">
-                    <h4 className="text-2xl md:text-3xl font-medium leading-7 mb-4 ">The Sandbar & Alligator Lighthouse</h4>
+                    <h4 className="text-2xl md:text-3xl medium leading-7 mb-4 ">The Sandbar & Alligator Lighthouse</h4>
                     <p className="text-sm md:text-base mb-2">This tour is fun for all ages. Take a short ride to the famous Islamorada sandbar where you can relax and play all day in the warm shallow water.  On the weekend, this is where the party happens.</p>
                     <p className="text-sm md:text-base mb-4">Visit the Alligator Lighthouse a few minutes farther from shore. The gorgeous turquoise water makes it a great place swim and snorkel.</p>
-                    <p className="text-sm md:text-base font-bold mb-4">Availability: All year</p>
+                    <p className="text-sm md:text-base bold mb-4">Availability: All year</p>
                     <p className="text-xs mb-6">Rates vary depending on the boat and number of people.</p>
-                    <Link to="/contact" className="text-white font-medium bg-cyan-420 hover:bg-cyan-hover transition duration-300 ease-in-out rounded-3xl px-5 py-3.5 w-fit">Book A Charter</Link>
+                    <Link to="/contact" className="text-white medium bg-cyan-420 hover:bg-cyan-hover transition duration-300 ease-in-out rounded-3xl px-5 py-3.5 w-fit">Book A Charter</Link>
                 </div>
             </div>
         </>
