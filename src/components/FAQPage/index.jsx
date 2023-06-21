2f277b49d0b5772de1b2dd4737505a484ee65319
gabewolford <gabewolford@gmail.com>
2023-06-21 11:50:30 -0700

    fixed top padding on FAQs page

--- src/components/FAQPage/index.jsx
+++ src/components/FAQPage/index.jsx
@@ -6,7 +6,7 @@ export default function FAQPage() {
 
     return (
         <>
-            <div className="mx-auto max-w-7xl pt-4 md:pt-24 animate__animated animate__fadeIn">
+            <div className="mx-auto max-w-7xl pt-16 md:pt-24 animate__animated animate__fadeIn">
                 <div className="mx-6 lg:mx-20 mb-0 md:mb-8">
                     <Helmet>
                         <title>Islamorada Fishing | FAQs</title>
